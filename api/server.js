6349bb5111208ce11c370a5a63c4185a88d4b3c8
Schuyler <skym1n0r91@gmail.com>
2022-03-10 16:37:29 -0600

    posts id

--- api/server.js
+++ api/server.js
@@ -16,16 +16,27 @@ server.get("/api/posts", (req, res) => {
     .catch((error) => {
       console.log(error);
       res.status(500).json({
-        message: "error retrieving posts",
+        message: "The posts information could not be retrieved",
       });
     });
 });
 
-server.get("/", (req, res) => {
-  res.send(`
-    <h2>Shelter API</h>
-    <p>Welcome to the Shelter API</p>
-  `);
+server.get("/api/posts/:id", (req, res) => {
+  Posts.findById(req.params.id)
+    .then((posts) => {
+      if (posts) {
+        res.status(200).json(posts);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist" });
+      }
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .json({ message: "The post information could not be retrieved" });
+    });
 });
 
 module.exports = server;
