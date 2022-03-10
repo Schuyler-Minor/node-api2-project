aa95fe724a5ebae790f64f208e8814f0d450037f
Schuyler <skym1n0r91@gmail.com>
2022-03-10 16:25:25 -0600

    get api/posts

--- api/server.js
+++ api/server.js
@@ -1,2 +1,31 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+const Posts = require("./posts/posts-model");
+
+server.get("/api/posts", (req, res) => {
+  Posts.find(req.query)
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "error retrieving posts",
+      });
+    });
+});
+
+server.get("/", (req, res) => {
+  res.send(`
+    <h2>Shelter API</h>
+    <p>Welcome to the Shelter API</p>
+  `);
+});
+
+module.exports = server;
